f91add8e42766f72a703e30491852dce2c31b5cd
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-10 22:43:56 +0200

    ...

--- console.py
+++ console.py
@@ -5,7 +5,8 @@ this is the consoles module
 
 import cmd
 import re
-from shlex import split
+import shlex
+import ast
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
