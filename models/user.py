fa9672b635b12b41f9781e21e9f384515bdf5271
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-11 19:40:37 +0200

    ...

--- models/user.py
+++ models/user.py
@@ -6,6 +6,10 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """
     the above mentioned class
+    Attributes  : email: string - empty string
+password: string - empty string
+first_name: string - empty string
+last_name: string - empty string
     """
 
     email = ""
