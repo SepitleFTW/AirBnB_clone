7662de75d53b494dd2276535cba6c12604363924
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-11 16:28:45 +0200

    ///

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,9 @@ which can inherit from it later on
 Some syntax errors were fixed by ChatGPT-3.5
 """
 
+import models
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -15,64 +15,46 @@ class BaseModel:
     BaseModel class defines common attributes and methods for other classes.
     """
 
-    def __init__(self, *noob, **noobs):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
         """
-        time_format = "%Year-%Month-%Day%Hour:%Minute:%Second:%f"
-        if noobs:
-            for key, value in noobs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
+        time_format = "%Y-%M-%dT%Hr:%Min:%Sec.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for j, s in kwargs.items():
+                if j == "created_at" or s == "updated_at":
+                    self.__dict__[k] = datetime.steptime(s, time_format)
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
 
     def save(self):
         """
-        Updates the updated_at attribute with the current datetime.
+        update the class at teh current time
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dict for the BaseModel instance.
+        return dict to basemodel
+        instance
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.created_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
 
     def __str__(self):
         """
-        Returns a string for
-        """
-        return "[{}] ({}) {}".format(class_name, self.idl, self.__dict__)
-    if __name__ == "__main__":
-        my_model = BaseModel()
-        my_model_name = "Initial Model"
-        my_model.my_number = 69
-        print(my_model)
-        my_model.save()
-        print(my_model)
-        my_model_json = my_model.to_dict()
-        print(my_model_json)
-        print("JSON of my_model:")
-        for key in my_model_json.keys():
-            print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                           my_model_json[key]))
+        bring back str representation
+        of Basemodel"""
 
-        print("---")
-        my_new_model = BaseModel(**my_model_json)
-        print(my_new_model.id)
-        print(my_new_model)
-        print(type(my_new_model.created_at))
-
-        print("---")
-        print(my_model is my_new_model)
+        clname = self.__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
